8f0fa0891f9803652fd1e94f68f53352ab128185
Vaibhav Pandey <contact@vaibhavpandey.com>
2022-06-19 12:44:49 +0530

    Open WhatsApp if not already.

--- scripts/worker.js
+++ scripts/worker.js
@@ -20,12 +20,18 @@ browser.runtime.onMessage.addListener(function ({ type, href }) {
     if (type !== "open_wame_link") return;
     browser.tabs.query({ url: "*://web.whatsapp.com/*" })
         .then(function ([tab]) {
-            if (!tab) return;
-            browser.tabs.update(tab.id, { active: true, highlighted: true });
-            browser.scripting.executeScript({
-                target: { tabId: tab.id },
-                func: startChat,
-                args: [href],
-            });
+            if (!!tab) {
+                browser.tabs.update(tab.id, { active: true });
+                browser.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    func: startChat,
+                    args: [href],
+                });
+            } else {
+                browser.tabs.create({
+                    active: true,
+                    url: href,
+                });
+            }
         });
 });
