3882673ee20a90cde286a6a347b5ef4e0f7b39cd
Vaibhav Pandey <contact@vaibhavpandey.com>
2022-06-19 12:41:30 +0530

    Restructure JS files.

--- scripts/clickjack.js
+++ scripts/clickjack.js
@@ -1,3 +1,18 @@
-import { setUpClickHandler } from "./common";
+import browser from "webextension-polyfill";
 
-setUpClickHandler();
+(function () {
+    if (document.documentElement.getAttribute("data-wame") === "hooked") return;
+
+    document.documentElement.addEventListener("click", function (e) {
+        if (e.target.tagName !== "A") return;
+        const href = e.target.getAttribute("href");
+        if (!href.startsWith("https://wa.me/")) return;
+        e.preventDefault();
+        browser.runtime.sendMessage({
+            type: "open_wame_link",
+            href,
+        });
+    });
+
+    document.documentElement.setAttribute("data-wame", "hooked");
+})();
--- scripts/worker.js
+++ scripts/worker.js
@@ -1,5 +1,20 @@
-import { startChat } from "./common";
 import browser from "webextension-polyfill";
+
+function startChat(link) {
+    const existing = document.getElementById('opener');
+    if (!!existing) {
+        existing.setAttribute('href', link);
+        existing.click();
+    } else {
+        const a = document.createElement('a');
+        a.id = 'opener';
+        a.style.display = 'none';
+        a.setAttribute('href', link);
+        const header = document.getElementsByTagName('header')[0];
+        header.appendChild(a);
+        a.click();
+    }
+}
 
 browser.runtime.onMessage.addListener(function ({ type, href }) {
     if (type !== "open_wame_link") return;
